44dc2e73a344910f6254d30d33b4daa444c8b1e9
Rhema <reymarmcoc@gmail.com>
2026-07-04 23:00:22 +0100

    mobile dome fix

--- src/components/MobileDome.jsx
+++ src/components/MobileDome.jsx
@@ -4,7 +4,7 @@ import DomeGallery from "./DomeGallery";
 export const MobileDome = () => {
   return (
     <div className=" overflow-x-hidden max-md:w-screen max-md:h-[50vh] w-[80vw] h-[80vh]">
-      <DomeGallery fit={0.25} minRadius={190} segments={16} />
+      <DomeGallery fit={0.25} minRadius={210} segments={16} />
     </div>
   );
 };
